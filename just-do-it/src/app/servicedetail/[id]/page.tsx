2e3ad5c77f6065e091cc4756b06ac8baab59c11f
RodrigoFernandez <rodd_2610@hotmail.com>
2024-11-15 16:11:19 -0300

    UserProfile Cambio de rutas

--- just-do-it/src/app/servicedetail/[id]/page.tsx
+++ just-do-it/src/app/servicedetail/[id]/page.tsx
@@ -4,17 +4,17 @@ import { gymClasses } from '@/lib/utils';
 import ActivityDetail from '@/component/classDetail';
 
 export default function ClassDetailPage() {
-  const { id } = useParams(); // Obtiene el `id` de la URL como parámetro dinámico
+  const { id } = useParams(); 
 
-  // Busca la clase en el array gymClasses usando el id de la URL
+
   const gymClass = gymClasses.find((classItem: { id: string }) => classItem.id === id);
 
-  // Si no se encuentra la clase, muestra un mensaje
+  
   if (!gymClass) {
     return <p>Clase no encontrada</p>;
   }
 
-  // Renderiza el componente ActivityDetail con los datos de la clase encontrada
+ 
   return (
     <div className="flex justify-center items-center min-h-screen nim-w-screen ">
     <ActivityDetail 
---
+++ just-do-it/src/app/userprofile/index.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { useSession, signOut } from "next-auth/react";
+
+const ProfilePage = () => {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return <div>You must be logged in to view your profile.</div>;
+  }
+
+  return (
+    <div className="max-w-lg mx-auto p-4 bg-white rounded shadow-md">
+      <h1 className="text-2xl font-semibold text-center">User Profile</h1>
+      <div className="mt-4">
+        <div className="flex justify-center">
+          <img
+            src={session.user?.image}
+            alt="User Avatar"
+            className="w-24 h-24 rounded-full"
+          />
+        </div>
+        <div className="mt-4">
+          <h2 className="text-xl">Nombre: {session.user?.name}</h2>
+          <p className="text-xl">Correo electronico: {session.user?.email}</p>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
