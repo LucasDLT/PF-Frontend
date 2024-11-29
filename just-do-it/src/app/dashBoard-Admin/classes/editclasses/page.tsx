567696486a060f513d1a01056930a55d707179d6
Sebas de la Torre <lucasebasdlt@gmail.com>
2024-11-29 01:07:31 -0300

    elimine los estados locales por errores en deploy, indicaban que no estaban en uso

--- just-do-it/src/app/dashBoard-Admin/classes/editclasses/page.tsx
+++ just-do-it/src/app/dashBoard-Admin/classes/editclasses/page.tsx
@@ -3,16 +3,7 @@
 import React, { useState } from 'react';
 
 function EditEvent() {
-  {/*const [title, setTitle] = useState('');
-  const [eventDate, setEventDate] = useState('');
-  const [eventLocation, setEventLocation] = useState('');
-  const [stock, setStock] = useState('');
-  const [price, setPrice] = useState('');
-  const [description, setDescription] = useState('');
-  const [images, setImages] = useState<string[] | null>(['']); // Cambiado de string | null a string[]
 
-  const [eventAddress, setEventAddress] = useState('');
-* LO DEJO COMENTADO PORQUE NO ESTA EN USO */}
   return (
     <div>
       <h1>Editar Clases</h1>
