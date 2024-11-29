005974b8e78331bfb282bbb87e1c1e2b04bf82bb
Sebas de la Torre <lucasebasdlt@gmail.com>
2024-11-29 13:37:26 -0300

    se deja comentado el provider que causa error en deploy

--- just-do-it/src/component/provider/index.ts
+++ just-do-it/src/component/provider/index.ts
@@ -1,7 +1,8 @@
-'use client';
+{/*'use client';
 
 import React from 'react';
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
+*/}
