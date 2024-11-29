5bc25020ec0950c023736ab676db9bed0bcdffd8
Sebas de la Torre <lucasebasdlt@gmail.com>
2024-11-29 00:45:41 -0300

    modificacion en ubicacion de dependencia en useeffect, estaba fuera del parentesis

--- just-do-it/src/app/clear-session/page.tsx
+++ just-do-it/src/app/clear-session/page.tsx
@@ -9,11 +9,11 @@ import { useEffect } from 'react';
 function ClearSession() {
   const { logout } = useAuth();
 
-  useEffect(() => {
+useEffect(() => {
     logout();
     signOut({ callbackUrl: '/' });
-  }),
-    [];
+  }, []);
+    
 
   return <div></div>;
 }
