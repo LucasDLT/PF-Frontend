854737787587b6cdb3006d3f4bd6470dd7012542
Sebas de la Torre <lucasebasdlt@gmail.com>
2024-11-29 10:22:25 -0300

    elimino regla de eslint, se eliminan variables y funciones sin uso en membership admin

--- just-do-it/src/app/dashBoard-Admin/membership/page.tsx
+++ just-do-it/src/app/dashBoard-Admin/membership/page.tsx
@@ -3,17 +3,6 @@
 import { useState, useEffect } from 'react';
 import { useAuth } from '@/context';
 
-{/*import { ScrollArea } from '@/components/ui/scroll-area';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';*/}
-
-import { signOut } from 'next-auth/react';
-
 type AdminDonation = {
   id: string;
   email: string;
@@ -27,13 +16,11 @@ const port = process.env.NEXT_PUBLIC_APP_API_PORT;
 
 export default function AdminDonaciones() {
   const [donations, setDonations] = useState<AdminDonation[]>([]);
-  const [filteredDonations, setFilteredDonations] = useState<AdminDonation[]>(
-    [],
-  );
-  const [statusFilter, setStatusFilter] = useState<
+
+  const [statusFilter] = useState<
     'todas' | 'active' | 'pending'
   >('todas');
-  const [nameFilter, setNameFilter] = useState('');
+  const [nameFilter] = useState('');
   const { userSession, token, logout } = useAuth();
 
   const fetchDonations = async (): Promise<void> => {
@@ -62,73 +49,8 @@ export default function AdminDonaciones() {
     fetchDonations();
   }, [userSession]);
 
-  const handleConfirmPayment = async (id: string) => {
-    try {
-      const response = await fetch(
-        `http://localhost:${port}/auth/payment/donation/confirm/${id}`,
-        {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      );
-      if (response.status === 441) {
-        
-        logout()
-        signOut({ callbackUrl: '/' });
-      }
-      if (response.ok) {
-        await fetchDonations();
-        
-      } else {
-        console.error('Error confirming payment:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Error confirming payment:', error);
-    }
-  };
+  
 
-  const handleCancelPayment = async (id: string) => {
-    try {
-      const response = await fetch(
-        `http://localhost:${port}/auth/payment/donation/reject/${id}`,
-        {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      );
-
-      if (response.status === 441) {
-       
-        logout()
-        signOut({ callbackUrl: '/' });
-      }
-
-      if (response.ok) {
-        await fetchDonations();
-        
-      } else {
-        console.error('Error canceling payment:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Error canceling payment:', error);
-    }
-  };
-
-  useEffect(() => {
-    const filtered = donations.filter((donation) => {
-      if (statusFilter === 'active') return donation.status === 'active';
-      if (statusFilter === 'pending') return donation.status === 'pending';
-      return true;
-    });
-
-    setFilteredDonations(filtered);
-  }, [statusFilter, nameFilter, donations]);
 
   return (
     <div>
