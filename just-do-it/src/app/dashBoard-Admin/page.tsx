c91069f8daf4336c76d5748f10c190f3ac749665
Sebas de la Torre <lucasebasdlt@gmail.com>
2024-11-29 14:39:44 -0300

    se elimina codigo innecesario de la vista

--- just-do-it/src/app/dashBoard-Admin/page.tsx
+++ just-do-it/src/app/dashBoard-Admin/page.tsx
@@ -2,23 +2,18 @@
 
 import {
   CalendarIcon,
-  CreditCardIcon,
   MailIcon,
   MapPinIcon,
   PhoneIcon,
   UserIcon,
 } from 'lucide-react';
-import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { useRouter } from 'next/navigation';
-import { LuAlertCircle } from 'react-icons/lu';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { useAuth } from '@/context';
 import styles from './PanelAdmin.module.css'; // Importamos el CSS adicional
 
 export default function PanelAdmin() {
   const { userSession } = useAuth();
-  const Router = useRouter();
 
   return (
     <div className="container mx-auto p-6">
@@ -78,104 +73,6 @@ export default function PanelAdmin() {
                       Tus Clases
                     </CardTitle>
                   </CardHeader>
-                  <CardContent className="p-4 overflow-y-auto max-h-[300px]">
-                    {userSession?.assistantEvents?.length > 0 ? (
-                      <ul className="space-y-4">
-                        {userSession.assistantEvents.map((assistantEvent) => (
-                          <li
-                            key={assistantEvent.eventId}
-                            className="bg-gray-50 p-4 rounded-lg shadow-sm"
-                          >
-                            <div className="flex justify-between items-center">
-                              <div>
-                                <h3 className="font-semibold text-gray-800">
-                                  {assistantEvent.title}
-                                </h3>
-                                <span className="text-sm text-gray-500">
-                                  {new Date(
-                                    assistantEvent.eventDate,
-                                  ).toLocaleDateString()}
-                                </span>
-                              </div>
-                              <Button
-                                onClick={() =>
-                                  Router.push(
-                                    `/eventdetail/${assistantEvent.eventId}`,
-                                  )
-                                }
-                                variant="outline"
-                                className="text-blue-600 hover:bg-blue-50 transition-colors duration-300"
-                              >
-                                Ver Evento
-                              </Button>
-                            </div>
-                          </li>
-                        ))}
-                      </ul>
-                    ) : (
-                      <div className="flex flex-col items-center border p-6 border-blue-400 rounded-lg shadow-xl bg-blue-50">
-                        <LuAlertCircle className="text-5xl text-blue-600 mb-4" />
-                        <h1 className="font-bold text-xl text-gray-800 text-center mb-2">
-                          Aun no has reservado ninguna clase
-                        </h1>
-                        <h2 className="text-gray-700 text-lg text-center mb-4">
-                          Que estas esperando ve a reservar la tuya
-                        </h2>
-                      </div>
-                    )}
-                  </CardContent>
-                </Card>
-
-                {/* Membresías */}
-                <Card className="shadow-md bg-white">
-                  <CardHeader className="border-b border-gray-200">
-                    <CardTitle className="text-xl font-semibold text-gray-800 flex items-center">
-                      <CreditCardIcon className="w-5 h-5 mr-2 text-blue-500" />
-                      Tus Membresías
-                    </CardTitle>
-                  </CardHeader>
-                  <CardContent className="p-4 overflow-y-auto max-h-[300px]">
-                    {userSession?.memberships?.length > 0 ? (
-                      <ul className="space-y-4">
-                        {userSession.memberships.map((membership) => (
-                          <li
-                            key={membership.id}
-                            className="bg-gray-50 p-4 rounded-lg shadow-sm"
-                          >
-                            <div className="flex justify-between items-center">
-                              <div>
-                                <h3 className="font-semibold text-gray-800">
-                                  {membership.title}
-                                </h3>
-                                <span className="text-sm text-gray-500">
-                                  Válido hasta:{" "}
-                                  {new Date(
-                                    membership.expirationDate,
-                                  ).toLocaleDateString()}
-                                </span>
-                              </div>
-                              <Button
-                                variant="outline"
-                                className="text-blue-600 hover:bg-blue-50 transition-colors duration-300"
-                              >
-                                Renovar
-                              </Button>
-                            </div>
-                          </li>
-                        ))}
-                      </ul>
-                    ) : (
-                      <div className="flex flex-col items-center border p-6 border-blue-400 rounded-lg shadow-xl bg-blue-50">
-                        <LuAlertCircle className="text-5xl text-blue-600 mb-4" />
-                        <h1 className="font-bold text-xl text-gray-800 text-center mb-2">
-                          No tienes ninguna membresía activa
-                        </h1>
-                        <h2 className="text-gray-700 text-lg text-center mb-4">
-                          Considera obtener una para beneficios exclusivos
-                        </h2>
-                      </div>
-                    )}
-                  </CardContent>
                 </Card>
               </div>
             </CardContent>
