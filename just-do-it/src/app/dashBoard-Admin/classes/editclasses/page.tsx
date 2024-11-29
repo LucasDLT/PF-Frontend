71492ea1b35552c7dfb90b0802c243c5b7700189
Sebas de la Torre <lucasebasdlt@gmail.com>
2024-11-29 02:02:08 -0300

    restaure las variables sin uso y a su vez probamos uncodigo en slint para el off de la regla de unused vars

--- just-do-it/src/app/dashBoard-Admin/classes/editclasses/page.tsx
+++ just-do-it/src/app/dashBoard-Admin/classes/editclasses/page.tsx
@@ -3,8 +3,17 @@
 import React, { useState } from 'react';
 
 function EditEvent() {
+  const [title, setTitle] = useState('');
+  const [eventDate, setEventDate] = useState('');
+  const [eventLocation, setEventLocation] = useState('');
+  const [stock, setStock] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+  const [images, setImages] = useState<string[] | null>(['']); // Cambiado de string | null a string[]
 
-  return (
+  const [eventAddress, setEventAddress] = useState('');
+
+  return ( 
     <div>
       <h1>Editar Clases</h1>
     </div>
