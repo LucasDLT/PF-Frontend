fc8a62b6837f205ccccbef6280082cc1b1fcb3da
RodrigoFernandez <rodd_2610@hotmail.com>
2024-12-03 16:03:50 -0300

    Cositas

---
+++ just-do-it/src/app/not-autorized/page.tsx
@@ -0,0 +1,9 @@
+export default function NotAuthorizedPage() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>No tienes permiso para acceder a esta página</h1>
+        <p>Por favor, contacta al administrador si crees que esto es un error.</p>
+      </div>
+    );
+  }
+  
