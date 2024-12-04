92ffa57c4bda22cec1ec91b9d759d4419639dfb4
Sebas de la Torre <lucasebasdlt@gmail.com>
2024-12-04 13:13:36 -0300

    se comenta codigo por las dudas

--- just-do-it/src/lib/porlasdudas.tsx
+++ just-do-it/src/lib/porlasdudas.tsx
@@ -1,4 +1,4 @@
-'use client';
+{/*'use client';
 
 import { useState, useEffect, useCallback } from 'react';
 import { Button } from '@/components/ui/button';
@@ -289,3 +289,4 @@ export default function AdminClassEditor() {
     </div>
   );
 }
+*/}
