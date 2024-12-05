b192677b5c3608d5b9e36430deff1d9707f71246
Sebas de la Torre <lucasebasdlt@gmail.com>
2024-12-05 16:32:12 -0300

    notificaciones de clases al cancelar y sonner

--- just-do-it/src/component/customConfirm/index.tsx
+++ just-do-it/src/component/customConfirm/index.tsx
@@ -8,15 +8,17 @@ interface ConfirmDialogProps {
   onConfirm: (arg: any) => void;
   title: string;
   description: string;
+  message?: string;
 }
 
-export function ConfirmDialog({ isOpen, onClose, onConfirm, title, description }: ConfirmDialogProps) {
+export function ConfirmDialog({ isOpen, onClose, onConfirm, title, description, message}: ConfirmDialogProps) {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
           <DialogDescription>{description}</DialogDescription>
+          <DialogDescription>{message}</DialogDescription>
         </DialogHeader>
         <DialogFooter>
           <Button variant="outline" onClick={onClose}>Cancelar</Button>
