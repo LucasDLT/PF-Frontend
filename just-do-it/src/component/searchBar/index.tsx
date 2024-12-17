8ab0823b4a6e5efc20d1553c717e62476f211cd0
RodrigoFernandez <rodd_2610@hotmail.com>
2024-12-16 21:13:40 -0300

    cambio en la vista de todas las clases

--- just-do-it/src/component/searchBar/index.tsx
+++ just-do-it/src/component/searchBar/index.tsx
@@ -1,16 +1,15 @@
 'use client';
 import { useState } from 'react';
 import { Input } from '@/components/ui/input';
-import CardClass from '../card-class';
+import CardClass from '@/component/card-class';
 import styles from './searchbar.module.css';
 import { useAuth } from '@/context';
 
 export default function SearchBar() {
-  const { classes } = useAuth(); 
+  const { classes } = useAuth();
   const [searchTerm, setSearchTerm] = useState('');
   const [filterMethod, setFilterMethod] = useState('name');
 
-  
   const filteredClasses = classes?.filter((gymClass) => {
     if (filterMethod === 'name') {
       return gymClass.name.toLowerCase().includes(searchTerm.toLowerCase());
@@ -24,27 +23,24 @@ export default function SearchBar() {
     <div className={styles.searchBarContainer}>
       <h1 className={styles.titleSearchBar}>Buscar Clases de Gimnasio</h1>
 
-      
       <Input
         type="text"
         placeholder={`Buscar por ${filterMethod === 'name' ? 'nombre de clase' : 'sala'}...`}
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        className={`${styles.input} mb-6`} 
+        className={styles.input}
       />
 
-    
       <div className={styles.cardsContainer}>
         {filteredClasses.map((gymClass) => (
           <CardClass key={gymClass.id} gymClass={gymClass} />
         ))}
+        {filteredClasses.length === 0 && (
+          <p className={styles.noResults}>
+            No se encontraron clases que coincidan con la búsqueda.
+          </p>
+        )}
       </div>
-
-      {filteredClasses.length === 0 && (
-        <p className="text-center text-gray-500 mt-4">
-          No se encontraron clases que coincidan con la búsqueda.
-        </p>
-      )}
     </div>
   );
 }
